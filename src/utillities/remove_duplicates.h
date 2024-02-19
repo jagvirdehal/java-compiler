12b31a0467f562c479c057cf5cbdc4236086bdfd
Aryaman Dhingra <a23dhing@uwaterloo.ca>
2024-02-18 23:40:21 -0500

    Override remove duplicates method

--- src/utillities/remove_duplicates.h
+++ src/utillities/remove_duplicates.h
@@ -11,4 +11,22 @@ template <typename T> void removeDuplicates(std::vector<T>& vec) {
     vec.erase(last, vec.end());
 };
 
+void removeDuplicates(std::vector<TypeDeclaration> &vec) {
+     std::sort(vec.begin(), vec.end(), [](const TypeDeclaration& lhs, const TypeDeclaration& rhs) {
+        auto getIdentifier = [](const auto& type) -> const std::string& {
+            return type->identifier;
+        };
+        
+        const std::string& id1 = std::visit(getIdentifier, lhs);
+        const std::string& id2 = std::visit(getIdentifier, rhs);
+
+        return id1 < id2;
+    });
+
+    vec.erase(std::unique(vec.begin(), vec.end(), 
+    [](const TypeDeclaration& lhs, const TypeDeclaration& rhs) {
+        return std::visit([](const auto& type) { return type->identifier; }, lhs) == std::visit([](const auto& type) { return type->identifier; }, rhs);
+    }), vec.end());
+};
+
 };
